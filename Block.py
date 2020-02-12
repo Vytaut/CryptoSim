d4d1f9b8a623f79e3df505457a3234a6421be41f
Witold Serwatka <witold.serwatka@pm.me>
2020-02-12 12:05:39 +0100

    added block validation function

--- Block.py
+++ Block.py
@@ -2,13 +2,15 @@ import hashlib
 
 class Block:
 
-    def __init__(self,index,timestamp,data,hash,prevhash):
+    def __init__(self,index,timestamp,data,prevhash):
         self.index = index
         self.timestamp = timestamp
         self.data = data
-        self.hash = hash
-        self.prevHash = prevhash
+        self.prevhash = prevhash
+        self.hash = self.calculateHash()
 
-    def calculateHash(self,index,timestamp,data,prevhash):
-        hash = str(index) + str(timestamp) + str(data) + str(prevhash)
+    def calculateHash(self):
+        hash = str(self.index) + str(self.timestamp) + str(self.data) + str(self.prevhash)
         return hashlib.sha256(hash.encode())
+
+#TODO: genesis block
---
+++ Blockchain.py
@@ -0,0 +1,32 @@
+import time
+
+import Block as bl
+
+
+class BlockChain:
+    def __init__(self):
+        self.blockChain = [self.generateGenesisBlock()]
+
+    def generateGenesisBlock(self):
+        return bl.Block(0, time.time(), None, None)
+
+    def generateNextBlock(self, blockdata):
+        prevblock = self.getLastBlock()
+        index = prevblock.index + 1
+        prevhash = prevblock.calculateHash()
+        return bl.Block(index, time.time(), blockdata, prevhash)
+
+    def getLastBlock(self):
+        return self.blockChain[-1]
+
+    def validateBlock(self, prevblock, nextblock):
+        if prevblock.index + 1 != nextblock.index:
+            print('Invalid block index!')
+            return False
+        if prevblock.calculateHash() != nextblock.prevhash:
+            print('Invalid previous block hash!')
+            return False
+        if nextblock.calculateHash() != nextblock.hash:
+            print('Invalid block hash!')
+            return False
+        return True
