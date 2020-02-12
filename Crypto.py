cf869904f0ac1715107eb21d5582dda77e55ccfe
Witold Serwatka <witold.serwatka@pm.me>
2020-02-12 18:03:10 +0100

    added chain replacement method

--- Crypto.py
+++ Crypto.py
@@ -8,8 +8,15 @@ class Crypto:
         self.chain = chain
 
     def validateChain(self):
+        #TODO: validate genesis block
         for i in range(1, len(self.chain.blockChain)):
-            block = self.chain.blockChain[i]
-            if not block.validateBlock() or not self.chain.validateBlockSeq(self.chain.blockChain[i-1],block):
+            if not self.chain.blockChain[i].validateBlock() or not self.chain.validateBlockSeq(self.chain.blockChain[i-1],self.chain.blockChain[i]):
                 return False
         return True
+
+    def replaceChain(self,newChain: bc.BlockChain):
+        if len(self.chain.blockChain) < len(newChain.blockChain):
+            print('Replaced old chain with longer one')
+            self.chain = newChain
+        else:
+            print('New chain invalid')
